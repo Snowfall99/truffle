e025c5a3260154ead463fd70d460b6ba5ab3a479
Snowfall99 <445117445@qq.com>
2022-04-11 10:15:38 +0800

    front: replace bft with distributed

--- views/front.go
+++ views/front.go
@@ -9,7 +9,7 @@ import (
 func FrontPage(bfts []model.BFT) g.Node {
 	body := []g.Node{}
 	body = append(body, H1(g.Text(`Truffle Wiki`)))
-	body = append(body, P(g.Text(`Do you want to learn more about BFT consensus?`)))
+	body = append(body, P(g.Text(`Do you want to learn more about idstributed consensus?`)))
 	for _, bft := range bfts {
 		body = append(body, A(Href(bft.Name), g.Text(bft.Name), Br()))
 	}
